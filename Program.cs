2613771342f533e073225e13dae47ad0c9f50b09
abdulrahman ayman shampolia <42439047+abdoshampolia@users.noreply.github.com>
2024-06-01 00:51:43 +0300

    adding-app-base-common-and-helpers

--- Program.cs
+++ Program.cs
@@ -1,8 +1,25 @@
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using MoneyFellows.Core.Entities;
+using MoneyFellows.Infrastructure.Contexts;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
 builder.Services.AddControllers();
+
+builder.Services.AddDbContext<MoneyFellowsDbContext>(options =>
+       options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+
+builder.Services.AddIdentity<User, IdentityRole>()
+.AddEntityFrameworkStores<MoneyFellowsDbContext>()
+.AddDefaultTokenProviders();
+
+builder.Services.AddCors(config =>
+{
+    config.AddPolicy("AllowAll", p => p.AllowAnyOrigin().AllowAnyMethod().AllowAnyHeader());
+});
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
